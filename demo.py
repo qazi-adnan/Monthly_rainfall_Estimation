db23259b24fe89c8f0b829a8d9e35e44c96f10de
qazi_adnan <qazi.adnan.2k1@gmail.com>
2025-02-07 02:37:26 +0530

    ready

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from monthly_rainfall_estimation.exception import RainfallException
-import sys
 
-try:
-    a=2/0
-except Exception as e:
-    raise RainfallException( e , sys)
 
 from monthly_rainfall_estimation.logger import logging
-logging.info("This is a info message")
+logging.info("This is a info messahjkhge")
